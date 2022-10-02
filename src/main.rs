f6d25e48497d766773fc697662cc8dbddf31aac6
JC Holder <jc@thirdtruck.org>
2022-10-02 18:08:26 -0400

    List Looptober MP3 files found in the target Dropbox folder

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,13 @@ use rocket_include_static_resources::{EtagIfNoneMatch, StaticContextManager, Sta
 
 use simple_xml_builder::XMLElement;
 
+use dropbox_sdk::{files, UserAuthClient};
+use dropbox_sdk::default_client::UserAuthDefaultClient;
+
+use std::collections::VecDeque;
+
+use regex::Regex;
+
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
 struct OEmbed {
@@ -44,6 +51,47 @@ impl OEmbed {
     }
 }
 
+fn list_music_files<'a, T: UserAuthClient>(client: &'a T, folder_path: &str) -> Vec<dropbox_sdk::files::Metadata> {
+    let mp3_regex = Regex::new(r".*.mp3").unwrap();
+
+    let listed_files = files::list_folder(
+        client,
+        &files::ListFolderArg::new(folder_path.into()).with_recursive(true),
+    );
+
+    let result = listed_files.unwrap().unwrap();
+    let cursor = if result.has_more {
+        Some(result.cursor)
+    } else {
+        None
+    };
+
+    let buffer: VecDeque<dropbox_sdk::files::Metadata> = result.entries.into();
+
+    let directory = DirectoryIterator {
+        client,
+        cursor,
+        buffer,
+    };
+
+    let metadata: Vec<dropbox_sdk::files::Metadata> = directory.collect();
+
+    metadata
+        .iter()
+        .filter(|file| {
+            match file {
+                files::Metadata::File(entry) => {
+                    let entry = entry.clone();
+                    let filename = entry.path_display.unwrap_or(entry.name);
+                    mp3_regex.is_match(&filename)
+                },
+                _ => false,
+            }
+        })
+        .map(|file| file.clone())
+        .collect()
+}
+
 static_response_handler! {
     "/~jaycie/looptober-jaycie-2022-10-01.mp3" => looptober_jaycie_2022_10_01 => "looptober-jaycie-2022-10-01",
 }
@@ -63,10 +111,54 @@ fn oembed(username: &str, filename: &str) -> Json<OEmbed> {
 
 #[launch]
 fn rocket() -> _ {
+    let auth = dropbox_sdk::oauth2::get_auth_from_env_or_prompt();
+    let client = UserAuthDefaultClient::new(auth);
+
+    let folder_path = "/Looptober/2022/".to_string();
+
+    let music_files = list_music_files(&client, &folder_path);
+
+    for file in music_files {
+        match file {
+            files::Metadata::File(entry) => {
+                let filepath = entry.path_display.unwrap_or(entry.name);
+                println!("File: {}", filepath);
+            }
+            _ => {
+                println!("Unexpected metadata: {:?}", file);
+            }
+        }
+    }
+
     rocket::build()
         .attach(static_resources_initializer!(
             "looptober-jaycie-2022-10-01" => "music/looptober-2022-10-01.mp3",
             "readme" => "README.md",
         ))
         .mount("/", routes![oembed, looptober_jaycie_2022_10_01])
+}
+
+struct DirectoryIterator<'a, T: UserAuthClient> {
+    client: &'a T,
+    buffer: VecDeque<files::Metadata>,
+    cursor: Option<String>,
+}
+
+impl<'a, T: UserAuthClient> Iterator for DirectoryIterator<'a, T> {
+    type Item = files::Metadata;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(entry) = self.buffer.pop_front() {
+            Some(entry)
+        } else if let Some(cursor) = self.cursor.take() {
+            let result = files::list_folder_continue(self.client, &files::ListFolderContinueArg::new(cursor)).unwrap().unwrap();
+            self.buffer.extend(result.entries.into_iter());
+            if result.has_more {
+                self.cursor = Some(result.cursor);
+            }
+            self.buffer.pop_front().map(|entry| entry)
+        } else {
+            None
+        }
+    }
 }
